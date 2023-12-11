218556bfa12fabeaa3d9624743286077ed1d1467
Moh. Hasbi Assidiqi <mohhasbias@gmail.com>
2023-12-11 16:23:01 +0300

    add dependencies file. update incomplete list.

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -18,12 +18,8 @@ import streamlit as st
 """
 
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import folium
-from folium.plugins import HeatMap
-import datetime
 import os
 from fnmatch import fnmatch
 
